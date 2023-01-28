1c885371cc5159baba1e2e8498bccc417e1f8c3c
dalton <daltonchivambo@gmail.com>
2023-01-28 20:32:36 +0200

    Add: variavel.js

---
+++ Gugas/05 - variavel.js
@@ -0,0 +1,25 @@
+
+
+
+
+/*
+
+No javascript temos 3 palavras chaves para declarar variaveis
+-> var
+-> let
+-> const
+*/
+
+var a = 2;
+var b = 5;
+var c = a * b;
+console.log(c);
+
+
+var dataNascimento, nome, sobrenome, nomeCompleto;
+
+nome = "Dalton";
+sobrenome = "Chivambo";
+nomeCompleto = nome + " " + sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto

9ff2c3a76d3b4cfae4076285dd5c96d5276c9a6c
Called-Wen <wen.macie@gmail.com>
2023-01-28 20:53:21 +0200

    add: variaveis

---
+++ Wen/js/sintaxe.js
@@ -0,0 +1,23 @@
+//textos numeros numeros decimais caseSensitive
+
+console.log(5+5);
+
+console.log("5+5");
+
+console.log("1");
+
+console.log(1);
+
+var a=1;
+var b=2;
+var c = a+b;
+var d = a*b;
+
+var h =a>b;
+
+console.log(c);
+
+console.log(d);
+
+console.log(h);
+

e237c2f875c26fcab3d31d1efa6b1b5dbef9c5be
dalton <daltonchivambo@gmail.com>
2023-01-30 17:45:39 +0200

    Add: Operadores

---
+++ Gugas/06 - operadores.js
@@ -0,0 +1,33 @@
+
+
+
+var value1, value2, total;
+value1 = 5;
+value2 = 2;
+
+total = ++value1;
+
+/*O valor total value1
+E o novo value1 é value+1)
+*/
+
+console.log("Total is " + total)
+console.log("Value1 is " + value1)
+
+
+var a, b;
+a = 2
+b = 2
+// Verificar se o valor é o mesmo.
+equal = (a == b);
+console.log(equal);
+
+
+
+var c, d;
+c = 2
+d = "2"
+// Verificar se o valor é o mesmo e o seu tipo.
+equal = (c === d);
+console.log(equal);
+

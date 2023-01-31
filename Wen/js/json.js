04861425895c0d27508c2a06ede1968aa7cdb8ea
Called-Wen <wen.macie@gmail.com>
2023-01-31 19:46:20 +0200

    add: aula 17-18

--- Wen/js/json.js
+++ Wen/js/json.js
@@ -1,1 +1,8 @@
 //JSON(JavaScript Object Notation) é uma forma de converter texto em objecto ou objecto em texto. É lido praticamente por toda linguagem de programação. Existem dois métodos JSON.parse()  texto para objecto e JSON.stringify() objecto para texto
+
+const carro = {
+    marca: "Ford",
+    modelo: "Ranger",
+    ano: 2022
+
+}

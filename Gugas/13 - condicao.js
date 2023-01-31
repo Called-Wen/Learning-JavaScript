29aaff83bd083933de98a833e85daa37f98b0aa3
dalton <daltonchivambo@gmail.com>
2023-01-31 16:40:56 +0200

    update: condicao

--- Gugas/13 - condicao.js
+++ Gugas/13 - condicao.js
@@ -1,12 +1,11 @@
 function verificar(){
     var nome = document.getElementById("nome").value;
-
+    let p = document.getElementById("texto");
+    
     if(nome =="" || nome==null){
-        let p = document.getElementById("texto");
         p.innerHTML = "O campo não pode estar vazio!";
         p.style.color ="red"
     } else {
-        let p = document.getElementById("texto");
         p.innerHTML = "";
     }
 }

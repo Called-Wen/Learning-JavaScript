1575b02ce7c1329365b536ee73c6837df43cc7e8
dalton <daltonchivambo@gmail.com>
2023-01-31 18:41:02 +0200

    Add: ClearInterval

--- Gugas/16 - timeout_interval.js
+++ Gugas/16 - timeout_interval.js
@@ -3,10 +3,29 @@
 
 function iniciarContagem(){
 
-document.getElementById('tempo').innerHTML = "Comecou a contar!"
+// document.getElementById('tempo').innerHTML = "Comecou a contar!"
 
 //Executa apenas uma vez apos o tempo determinado.
-    setTimeout(function(){
-        document.getElementById('tempo').innerHTML = "Executou o setTimeout";
-    },1000);
-}
+
+//     setTimeout(function(){
+//         document.getElementById('tempo').innerHTML = "Executou o setTimeout";
+//     },1000);
+//
+
+
+
+tempo = setInterval(function(){
+    var cronometro = document.getElementById('tempo').innerHTML
+    var soma = parseInt(cronometro) +1;
+    document.getElementById('tempo').innerHTML = soma
+},1000)
+
+ }
+
+
+
+
+
+ function pausaContagem(){
+    clearInterval(tempo)
+ }

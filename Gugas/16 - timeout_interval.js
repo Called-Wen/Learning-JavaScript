df76bfa74236d1332a8ddaf39c38140330e9bf28
dalton <daltonchivambo@gmail.com>
2023-01-31 18:44:53 +0200

    update

--- Gugas/16 - timeout_interval.js
+++ Gugas/16 - timeout_interval.js
@@ -1,31 +1,31 @@
 
 
 
-function iniciarContagem(){
+function iniciarContagem() {
 
-// document.getElementById('tempo').innerHTML = "Comecou a contar!"
+    // document.getElementById('tempo').innerHTML = "Comecou a contar!"
 
-//Executa apenas uma vez apos o tempo determinado.
+    //Executa apenas uma vez apos o tempo determinado.
 
-//     setTimeout(function(){
-//         document.getElementById('tempo').innerHTML = "Executou o setTimeout";
-//     },1000);
-//
-
-
-
-tempo = setInterval(function(){
-    var cronometro = document.getElementById('tempo').innerHTML
-    var soma = parseInt(cronometro) +1;
-    document.getElementById('tempo').innerHTML = soma
-},1000)
-
- }
+    //     setTimeout(function(){
+    //         document.getElementById('tempo').innerHTML = "Executou o setTimeout";
+    //     },1000);
+    //
 
 
 
+    tempo = setInterval(function () {
+        var cronometro = document.getElementById('tempo').innerHTML
+        var soma = parseInt(cronometro) + 1;
+        document.getElementById('tempo').innerHTML = soma
+    }, 1000)
+
+}
 
 
- function pausaContagem(){
+
+
+
+function pausaContagem() {
     clearInterval(tempo)
- }
+}
